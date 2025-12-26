a02689eb742071249f226824b123cf75522a2750
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2025-12-26 21:11:07 +0700

    chore: change package build and remove slug route handler

--- api/runtime-config.ts
+++ api/runtime-config.ts
@@ -2,7 +2,8 @@ import { apiAuth } from "@/lib/auth";
 import { CreateClientConfig } from "./client/client.gen";
 
 export const createClientConfig: CreateClientConfig = (config) => {
-  const baseUrl = "/api";
+  // const baseUrl = "/api";
+  const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
 
   return {
     ...config,
@@ -11,6 +12,10 @@ export const createClientConfig: CreateClientConfig = (config) => {
       if (auth.type === "http" && auth.scheme === "bearer") {
         return apiAuth();
       }
+    },
+    headers: {
+      ...config?.headers,
+      "ngrok-skip-browser-warning": true
     }
   };
 };
